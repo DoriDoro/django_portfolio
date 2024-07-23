88b7edb8cdef95960a417857eae69828c56331b9
dorothea <dorothea.reher@cocoonr.fr>
2024-07-23 12:35:30 +0200

    modified settings and urls

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 import sentry_sdk
 
+from django.utils.translation import gettext_lazy as _
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -62,6 +63,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -124,13 +126,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
+TIME_ZONE = 'Europe/Paris'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
-
+LANGUAGES = [
+    ("en", _("English")),
+    ("de", _("German")),
+    ("fr", _("French")),
+]
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,17 +16,21 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls", namespace="core")),
-    path("", include("doridoro.urls", namespace="doridoro")),
-    path("", include("projects.urls", namespace="projects")),
     path("tinymce/", include("tinymce.urls")),
 ]
+
+urlpatterns += i18n_patterns(
+    path("", include("doridoro.urls", namespace="doridoro")),
+    path("portfolio/", include("projects.urls", namespace="projects")),
+    prefix_default_language=False
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
