e788d77083dbd442310bdbad05e1ae1738f94d33
doridoro <dorothea.reher@gmail.com>
2024-08-07 15:28:13 +0200

    add language form

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,13 +23,14 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("tinymce/", include("tinymce.urls")),
 ]
 
 urlpatterns += i18n_patterns(
     path("", include("doridoro.urls", namespace="doridoro")),
     path("portfolio/", include("projects.urls", namespace="projects")),
-    prefix_default_language=False
+    prefix_default_language=False,
 )
 
 if settings.DEBUG:
