12e04b00bc5b3bbaca59dbe95bc8eef4378f1b6b
doridoro <dorothea.reher@gmail.com>
2024-09-30 20:23:37 +0200

    modified the portfolio photo view

--- projects/views.py
+++ projects/views.py
@@ -9,7 +9,9 @@ class PortfolioView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["projects"] = Project.objects.filter(published=True)
+        context["projects"] = Project.objects.filter(published=True).order_by(
+            "-create_date"
+        )
         return context
 
 
