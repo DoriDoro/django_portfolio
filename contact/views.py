2c19eee3298de67eb520b70afcc085a024cfc027
doro <dorothea.hassan@gmail.com>
2025-02-16 20:59:37 +0100

    implement message framework

--- contact/views.py
+++ contact/views.py
@@ -10,7 +10,7 @@ from contact.forms import ContactRequestForm
 class ContactRequestView(SuccessMessageMixin, FormView):
     form_class = ContactRequestForm
     template_name = "contact.html"
-    success_message = "Your message was sent."
+    success_message = "Thank you. Your message was sent."
     success_url = reverse_lazy("doridoro:index")
 
     def form_valid(self, form):
