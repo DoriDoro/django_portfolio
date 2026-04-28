1c3cefb8890316dfc614a44d15cfafa6677c5835
doro <dorothea.hassan@gmail.com>
2026-04-28 20:35:33 +0200

    refactor: add error templates

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,26 +1,10 @@
-"""
-URL configuration for portfolio project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.defaults import permission_denied, page_not_found, server_error
 
 from sitemaps import ProjectSitemap, JournalSitemap, StaticViewSitemap
 
@@ -40,7 +24,7 @@ sitemaps = {
 # translation enables urlpatterns:
 urlpatterns += i18n_patterns(
     path("", include("core.urls", namespace="core")),
-    # path("", include("doridoro.urls", namespace="doridoro")),
+    path("", include("doridoro.urls", namespace="doridoro")),
     path("portfolio/", include("projects.urls", namespace="projects")),
     path("contact/", include("contact.urls", namespace="contact")),
     path(
@@ -54,3 +38,14 @@ urlpatterns += i18n_patterns(
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += [
+        path(
+            "__preview__/403/",
+            permission_denied,
+            {"exception": Exception("Preview 403")},
+        ),
+        path(
+            "__preview__/404/", page_not_found, {"exception": Exception("Preview 404")}
+        ),
+        path("__preview__/500/", server_error),
+    ]
